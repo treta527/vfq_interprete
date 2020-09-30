55b9c521d04c5d0343b4adf1d7bcf39dd962d83f
treta527 <arn.leonardo@gmail.com>
2020-09-30 10:25:36 -0500

    se ajusta mecanismo para filtrar la salida del parser para solo permitir caracteres números letras comillas punto y llaves

--- ios/Classes/document.cpp
+++ ios/Classes/document.cpp
@@ -32,17 +32,18 @@ char *parse(char *rawData) {
     strcat(output, "\", \"placeBirth\": \"");
     strcat(output, p.placeBirth);
     strcat(output, "\"}");
-    int size = getSize(output);
+  int size = getSize(output);
     int ind = 0;
     char *response = new char[1024] {
         0
     };
     for (int i = 0; i < size; i++) {
         uint8_t ch = output[i];
-        if (ch < 0x80) {
-//            cout << output[i] << " ";
-            response[ind++] = output[i];
-        }
+         if (ch == 0x3A || ch >= 0x41 && ch <= 0x5A || ch >= 0x61 && ch <= 0x7A
+                         || ch == 0x7B || ch == 0x7D || ch == 0x22 || ch == 0x2C
+                         || ch == 0x20 || ch == 0x2B || ch == 0x2D || ch >= 0x30 && ch <= 0x39) {
+                    response[ind++] = output[i];
+                }
     }
     return response;
 }
